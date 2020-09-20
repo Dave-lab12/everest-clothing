68e20e0bde79cc6c8c600c4aff22d798b2c5791c
Dave <Dwtmekonnen123@gmail.com>
2020-09-20 15:47:34 +0300

    updating out app with a shop page,collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
+import ShopPages from "./pages/shop/shop.component";
 
-const HatsPage = () => (
-  <div>
-    <h1>hats page</h1>
-  </div>
-);
 function App() {
   return (
     <div>
       <switch>
         <Route exact path="/" component={Homepage} />
-        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPages} />
       </switch>
     </div>
   );
---
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./collection-preview.styles.scss";
+import CollectionItem from "../collection-item/collection-tem.component";
+
+const collectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...itemProps }) => (
+          <CollectionItem key={id} {...itemProps} />
+        ))}
+    </div>
+  </div>
+);
+
+export default collectionPreview;
